3dda86612c52ff626e85787b7532e77ecb0c8f94
Arley Wilches Marcelo <arley.wilches@gmail.com>
2015-04-06 23:17:41 -0500

    setyp

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='textalytics',
+      version='0.1',
+      description='textalytics',
+      url='https://github.com/arleincho/textalyticsSrc',
+      author='Arley Wilches',
+      author_email='arley.wilches@gmail.com',
+      license='MIT',
+      packages=['textalytics'],
+      zip_safe=False)
